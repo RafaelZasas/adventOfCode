60b76a0c1a139bba1a757a677009ce25d1704918
Rafael Zasas <zasas.rafi@gmail.com>
2024-12-06 13:36:24 -0800

    Optimize with go routines

--- 2024/q6/main.go
+++ 2024/q6/main.go
@@ -5,6 +5,7 @@ import (
 	"io"
 	"os"
 	"strings"
+	"sync"
 	"time"
 )
 
@@ -150,6 +151,8 @@ func partTwo() {
 	}()
 
 	validObstructions := 0
+	var wg sync.WaitGroup
+	resultChan := make(chan int, len(grid)*len(grid[0]))
 
 	for i, row := range grid {
 		for j, col := range row {
@@ -158,69 +161,99 @@ func partTwo() {
 				continue
 			}
 
-			// temp obstruction for this run
-			grid[i][j] = "#"
+			wg.Add(1)
 
-			guardRow := startingRow
-			guardCol := startingCol
-			guardDirection := "up"
+			// go forth and paralelllllelize this shit
+			go func(i, j int) {
+				defer wg.Done() // Decrement the counter when done
 
-			visited := make(map[string]bool)
-			inLoop := false
-
-			// move the guard around the grid to check if they end up in a loop
-			for steps := 0; steps < len(grid)*len(grid[0]); steps++ {
-
-				// check if the guard has already been in this position, and facing the
-				// same direction
-				state := fmt.Sprintf("%d,%d,%s", guardRow, guardCol, guardDirection)
-				if visited[state] {
-					inLoop = true
-					break
-				}
-				visited[state] = true
-
-				// same stuff as part one
-				if guardDirection == "up" {
-					if guardRow-1 < 0 || grid[guardRow-1][guardCol] == "#" {
-						guardDirection = "right"
-						continue
-					}
-					guardRow--
-				} else if guardDirection == "right" {
-					if guardCol+1 >= len(grid[0]) || grid[guardRow][guardCol+1] == "#" {
-						guardDirection = "down"
-						continue
-					}
-					guardCol++
-				} else if guardDirection == "down" {
-					if guardRow+1 >= len(grid) || grid[guardRow+1][guardCol] == "#" {
-						guardDirection = "left"
-						continue
-					}
-					guardRow++
-				} else if guardDirection == "left" {
-					if guardCol-1 < 0 || grid[guardRow][guardCol-1] == "#" {
-						guardDirection = "up"
-						continue
-					}
-					guardCol--
+				// avoiding race conditions by copying the grid
+				// there must be a better way to do this but
+				// Idgaf
+				localGrid := make([][]string, len(grid))
+				for k := range grid {
+					localGrid[k] = append([]string{}, grid[k]...)
 				}
 
-				// Stop if the guard leaves the grid bc obvs they are not in a loop
-				if guardRow == 0 || guardCol == 0 || guardRow == len(grid)-1 || guardCol == len(grid[0])-1 {
-					break
+				// Placing a large cylindrical object
+				// in the way of the guard to make him
+				// go in circles
+				localGrid[i][j] = "#"
+
+				guardRow := startingRow
+				guardCol := startingCol
+				guardDirection := "up"
+
+				visited := make(map[string]bool)
+				inLoop := false
+
+				for steps := 0; steps < len(localGrid)*len(localGrid[0]); steps++ {
+					// Track the guard's state (position + direction)
+					state := fmt.Sprintf("%d,%d,%s", guardRow, guardCol, guardDirection)
+					if visited[state] {
+						inLoop = true
+						break
+					}
+					visited[state] = true
+
+					// Determine the next move based on the guard's direction
+					if guardDirection == "up" {
+						if guardRow-1 < 0 || localGrid[guardRow-1][guardCol] == "#" {
+							guardDirection = "right"
+							continue
+						}
+						guardRow--
+					}
+
+					if guardDirection == "right" {
+						if guardCol+1 >= len(localGrid[0]) || localGrid[guardRow][guardCol+1] == "#" {
+							guardDirection = "down"
+							continue
+						}
+						guardCol++
+					}
+
+					if guardDirection == "down" {
+						if guardRow+1 >= len(localGrid) || localGrid[guardRow+1][guardCol] == "#" {
+							guardDirection = "left"
+							continue
+						}
+						guardRow++
+					}
+
+					if guardDirection == "left" {
+						if guardCol-1 < 0 || localGrid[guardRow][guardCol-1] == "#" {
+							guardDirection = "up"
+							continue
+						}
+						guardCol--
+					}
+
+					// Stop if the guard leaves the grid
+					// because he is a coward
+					if guardRow == 0 || guardCol == 0 || guardRow == len(localGrid)-1 || guardCol == len(localGrid[0])-1 {
+						break
+					}
 				}
-			}
 
-			// If the guard ends up in a loop, count this obstruction as valid
-			if inLoop {
-				validObstructions++
-			}
-
-			// get rid of the obstruction in this pos
-			grid[i][j] = "."
+				// Send result to channel if the guard is in a loop
+				if inLoop {
+					resultChan <- 1
+				} else {
+					resultChan <- 0
+				}
+			}(i, j)
 		}
+	}
+
+	// Wait for all Go routines to finish
+	go func() {
+		wg.Wait()
+		close(resultChan)
+	}()
+
+	for result := range resultChan {
+		validObstructions += result
 	}
 
 	fmt.Println("Valid obstructions: ", validObstructions)
